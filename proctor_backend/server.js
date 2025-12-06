36ad9c845e2b816fe1beda4bd45fec9ebef50017
singhyashe15 <singhyashraj574@gmail.com>
2025-12-06 23:42:32 +0530

    commit

--- proctor_backend/server.js
+++ proctor_backend/server.js
@@ -1,9 +1,19 @@
 import express from "express"
-const app = express();
+import { app, server } from './socket.js'
+import cors from "cors";
 
+app.use(express.json());
 const port = 3000 || process.env.PORT;
 
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow necessary methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow necessary headers
+  optionsSuccessStatus: 200
+}));
 
-app.listen(port,()=>{
+
+server.listen(port,()=>{
   console.log("server listening on port" + port);
 })
---
+++ proctor_backend/socket.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL, // Allow all origins (use specific domain in production)
+    methods: ["GET", "POST", "DELETE", "PUT"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on('debate-room', (debateRoomId) => {
+    console.log(debateRoomId)
+    if (socket.rooms.has(debateRoomId)) {
+      console.log(`Already in room: ${debateRoomId}`);
+      return; // prevent rejoin
+    }
+    socket.join(debateRoomId);
+  })
+
+  socket.on('sendMsg', ({ debateId, message }) => {
+    console.log(message);
+    
+    socket.to(debateId).emit('real-time-sync-message', message);
+
+    //testing phase
+  })
+})
+
+export { app, server };
